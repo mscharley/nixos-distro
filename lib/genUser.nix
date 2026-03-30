4af55736691eac3c1b08066d528fed69be7200dd
Matthew Scharley <matt@scharley.me>
2026-03-30 15:23:20 +1100

    fix: add support for user icons

--- lib/genUser.nix
+++ lib/genUser.nix
@@ -1,6 +1,7 @@
 userDefinitionFile: let
 	def = if builtins.typeOf userDefinitionFile == "path" then import userDefinitionFile else userDefinitionFile;
 	userName = if def ? "userName" then def.userName else baseNameOf userDefinitionFile;
+	icon = if def ? "icon" then def.icon else null;
 in {
 	inherit userName;
 
@@ -11,7 +12,18 @@ in {
 	in vars@{ pkgs, lib, ... }: {
 		imports = modules;
 
-		users.users.${userName} = user vars;
-		home-manager.users.${userName} = lib.mkIf (home-manager != null) home-manager;
+		config = lib.mkMerge [
+			{
+				users.users.${userName} = user vars;
+				home-manager.users.${userName} = lib.mkIf (home-manager != null) home-manager;
+			}
+			(lib.mkIf (icon != null) {
+				# https://discourse.nixos.org/t/setting-the-user-profile-image-under-gnome/36233/10
+				systemd.tmpfiles.rules = [
+					"f+ /var/lib/AccountsService/users/${userName}  0600 root root - [User]\\nIcon=/var/lib/AccountsService/icons/${userName}\\n"
+					"L+ /var/lib/AccountsService/icons/${userName}  - - - - ${icon}"
+				];
+			})
+		];
 	};
 }
