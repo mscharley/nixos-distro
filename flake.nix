0947685eec68f750c64d1c9a637dc8f6e16bcc4c
Matthew Scharley <matt@scharley.me>
2026-06-18 13:11:04 +1000

    chore: bump flake

--- flake.nix
+++ flake.nix
@@ -33,7 +33,8 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      # Pinned to a specific hash until the next version after 1.0.0 to get needed patchs for nixos-unstable
+      url = "github:nix-community/lanzaboote/001e560fffc8f0235e9db20ebeb4ccde0ade1caf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
