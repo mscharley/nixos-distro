5a806d876d432a57e1ae9653eddd1c3d9c2cfec4
Matthew Scharley <matt@scharley.me>
2026-07-04 00:06:37 +1000

    fix: enable oxygen as an available theme

--- modules/desktops/kde.nix
+++ modules/desktops/kde.nix
@@ -1,47 +1,56 @@
 { pkgs, desktop, ... }:
 {
-	imports = [
-		../roles/graphical.nix
-	] ++ (if (desktop == "kde") then [ ./display-managers/plasma-login-manager.nix ] else []);
+  imports = [
+    ../roles/graphical.nix
+  ]
+  ++ (if (desktop == "kde") then [ ./display-managers/plasma-login-manager.nix ] else [ ]);
 
-	# Enable the KDE Plasma Desktop Environment
-	services.desktopManager.plasma6.enable = true;
+  # Enable the KDE Plasma Desktop Environment
+  services.desktopManager.plasma6.enable = true;
 
-	# Disable some of the optional packages
-	environment.plasma6.excludePackages = with pkgs; [
-		# Disable X11 session
-		kdePackages.kwin-x11
+  # Disable some of the optional packages
+  environment.plasma6.excludePackages = with pkgs; [
+    # Disable X11 session
+    kdePackages.kwin-x11
 
-		# Don't include konsole in favour of kitty
-		kdePackages.konsole
-	];
+    # Don't include konsole in favour of kitty
+    kdePackages.konsole
+  ];
 
-	# Enable fcitx5 + mozc as a Japanese IME
-	i18n.inputMethod = {
-		enable = true;
-		type = "fcitx5";
-		fcitx5 = {
-			addons = with pkgs; [ fcitx5-mozc ];
-		};
-	};
+  # Enable fcitx5 + mozc as a Japanese IME
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [ fcitx5-mozc ];
+    };
+  };
 
-	# Addon some extra niceties
-	environment.systemPackages = with pkgs; [
-		# Desirable for updating flatpaks and fwupd
-		kdePackages.discover
+  # Addon some extra niceties
+  environment.systemPackages = with pkgs; [
+    # Desirable for updating flatpaks and fwupd
+    kdePackages.discover
 
-		# Desktop apps
-		kdePackages.kate
-		kdePackages.kcalc kdePackages.kcharselect kdePackages.kcolorchooser
-		# kdePackages.kamoso
-		kdiff3
+    # Desktop apps
+    kdePackages.kate
+    kdePackages.kcalc
+    kdePackages.kcharselect
+    kdePackages.kcolorchooser
+    # kdePackages.kamoso
+    kdiff3
 
-		# System admin tools
-		kdePackages.flatpak-kcm
-		kdePackages.ksystemlog kdePackages.isoimagewriter
+    # System admin tools
+    kdePackages.flatpak-kcm
+    kdePackages.ksystemlog
+    kdePackages.isoimagewriter
 
-		# Gnome compatibility
-		adwaita-icon-theme
-	];
-	programs.kdeconnect.enable = true;
+    # Oxygen
+    kdePackages.oxygen
+    kdePackages.oxygen-sounds
+    kdePackages.oxygen-icons
+
+    # Gnome compatibility
+    adwaita-icon-theme
+  ];
+  programs.kdeconnect.enable = true;
 }
